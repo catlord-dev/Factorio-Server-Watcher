a8c2aacc327ea0a9ace822a7b8ecb975521fb031
catlord-dev <catlord03012012151804@gmail.com>
2024-08-02 22:23:11 -0400

    get rid of scope arg

--- commands.py
+++ commands.py
@@ -3,7 +3,7 @@ from interactions import Extension, SlashContext, contexts, slash_command
 
 class SlashCommands(Extension):
     # Define a simple slash command
-    @slash_command(name="test23", description="Says meow!2", scopes=[612680043789025291])
+    @slash_command(name="test23", description="Says meow!2")
     async def hello(self, ctx: SlashContext):
         print("mew")
         await ctx.send(f"Hello, {ctx.author.mention} meow!2")
